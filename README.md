4b1ed27dde1e8f2ad696838abb7981093b52fd69
kunal-singhh <kunal.singh@freshworks.com>
2022-09-01 19:25:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Notion Article Repo for Solution References | Notion Articles Sync with Freshdesk Solutions 
+
+
+## Learnings 
+
+Freshdesk SDK
+Freshdesk API
+Notion API
+Crayons Library
+
+# Notes 
+
+- Can not invoke interface methods from modal hence pass them back to parent and then invoke interface methods 
+- instance methods can be used to send data between different instances modal <-> app 
---
+++ app/scripts/app.js
@@ -0,0 +1,69 @@
+var client;
+let headersList = {
+  "Notion-Version": "2022-06-28",
+  Authorization: "Bearer secret_g8x8qRjQydSjOLWaYSH9X1CdLtQdExfOhAG8k7mFNkv",
+  "Content-Type": "application/json",
+};
+
+init();
+
+async function init() {
+  client = await app.initialized();
+
+  // receive data from modal
+  client.instance.receive(async function (event) {
+    var data = event.helper.getData();
+    let contact = await client.data.get('contact')
+    let greeting = `
+    Hello ${contact.contact.first_name}, <br>
+      Thank you for reaching out. Sharing reference article/articles with you which will help you with your query.
+      <br><br>
+      --Solutions--
+      <br><br>
+    `;
+    console.log({contact,greeting})
+    try {
+      let response = await client.interface.trigger("click", {
+        id: "reply",
+        text: greeting + data.message,
+      });
+      await client.interface.trigger('showNotify', {type:'success' , message : 'Created Reply with Article Link'});
+    } catch (err) {
+      let response = client.interface.trigger("setValue", {
+        id: "editor",
+        text: "<br>" + data.message,
+      });
+      await client.interface.trigger('showNotify', {type:'success' , message : 'Added Article Link to Reply' , position:"end"});
+    }
+  });
+  client.events.on("app.activated", onAppActivated);
+}
+
+async function onAppActivated() {
+  document.querySelector("#apptext").innerHTML = "App Activated" + "<br><br>";
+  // let notes = await getNotes();
+  // renderNotes(notes)
+  console.log("Event Registered");
+  document.querySelector(".toggle-btn").addEventListener("click", function (e) {
+    launchModal("Article Repositoriess", "templates/index.html", {
+      key: "value",
+    });
+  });
+}
+
+async function launchModal(title, template, data) {
+  try {
+    let modal = await client.interface.trigger("showModal", {
+      title,
+      template,
+      data,
+    });
+    console.log(modal);
+    return modal;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// show notification
+// await client.interface.trigger('showNotify', {type:'success' , message : 'yayy'});
